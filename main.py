e910ac3c52062774804af944284d06cb666cff1a
IcaroPortela <91165759+IcaroPortela@users.noreply.github.com>
2021-11-03 23:29:10 +0000

    Dados novos

--- main.py
+++ main.py
@@ -1,55 +1,72 @@
 import sqlite3
 
-banco = sqlite3.connect("mydb.db", 100)
-print("...Arquivo criado com sucesso!")
-cur = banco.cursor()
-cursor = cur
+banco = sqlite3.connect("banco2.db")
+#print("Um novo arquivo foi criado!")
+cursor = banco.cursor()
 def conexao():
+  cursor = banco.cursor()
+  if cursor.connection == banco:
+    print("Conexão funcionando!")
+  else:
+    print("Falha na conexão!")
+
+def criarTabela():
   try:
-    cursor= banco.cursor()
-    cursor.connection == banco
-    print("Banco conectado!")
-  except ConnectionError:
-    print("Erro de conexão!")
-conexao()
-
-
-
-def CriarTabela():
-  try:
-    cursor = cur
-    cursor.execute("CREATE TABLE dados(id integer primary key, nome text, idade integer, email text)")
+    cursor.execute("create table dados(id integer primary key, nome text, idade integer)")
     print("Tabela criada com sucesso!")
-  except Exception:
-    print("Erro na criação de tabelas!")
-  
-def InsertData():
-  try:
-    lista = [(1,"Carlos",22,"carlos22@gmail.com")]
-    cursor = cur
-    cursor.executemany("insert into dados values(?,?,?,?)", lista)
-    print(lista, "\nDados inseridos com sucesso!")
   except:
-    print("Dados não inseridos!")
+    print("Não possível criar a tabela! :/")
 
-def Deletar():
-  x = int(input("Digite o número da linha que deseja deletar: "))
+def inserirDados():
   try:
-    cursor = cur
-    cursor.execute("Delete from dados where id=:row ", {"row": x})
-    print("Linha", x , " foi deletada com sucesso")
-    exibir()
+    linha = int(input("Digite o id: "))
+    nome = str(input("Digite o seu nome: "))
+    idade = int(input("Digite sua idade: "))
+    cursor.execute("insert into dados VALUES(?,?,?)",(linha, nome, idade))
+    print("Dados foram inseridos com sucesso!")
   except:
-    print("Erro ao deletar linha!")
+    print("Os dados não foram inseridos!")
 
-#CriarTabela()
-#InsertData()
-def exibir():
-  cursor = cur
-  cursor.execute("select * from dados where id=:number", {"number": 1})
-  for row in cursor:
-    print(row)
-#Deletar()
-exibir()
+def deletaDados():
+  try:
+    N = int(input("Digite um número: "))
+    cursor.execute("delete from dados where id=:Numero",{"Numero": N})
+    print("Dados deletados com sucesso!")
+  except:
+    print("Dados não foram deletados! :(")
+
+def atualizaDados():
+  try:
+    novoNome = str(input("Digite o nome: "))
+    posicao = int(input("Digite um número: "))
+    cursor.execute("update dados set nome =:name where id=:Posicao",{"name":novoNome,"Posicao":posicao})
+    print("Dados from atualizados com sucesso!")
+  except:
+    print("Dados não foram atualizados! :(")
+
+def exibirDados():
+  number = [0,1,2,3]
+  while True:
+    opcoes = int(input("Digite a opção que deseja exibir(0 até 3): "))
+    if number[0] == opcoes:
+      for row in cursor.execute("select * from dados"):
+        print(row)
+    elif number[1] == opcoes:
+      for row in cursor.execute("select id from dados"):
+        print(row)
+    elif number[2] == opcoes:
+      for row in cursor.execute("select nome from dados"):
+        print(row)
+    else:
+      for row in cursor.execute("select idade from dados"):
+        print(row)
+#inserirDados()
+#conexao()
+#criarTabela()
+#inserirDados()
+#deletaDados()
+atualizaDados()
+exibirDados()
+print(cursor.fetchone()[1])
 banco.commit()
 banco.close()
